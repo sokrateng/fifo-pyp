b6e2ecd590ef2a8709d200253038a88355c7c122
Engin Çoban <engin.coban@penta.com.tr>
2025-12-07 02:08:15 +0300

    chore: vercel config ve dist ayari

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import StockAgingSuiteV5 from './StockAgingSuiteV5.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <StockAgingSuiteV5 />
+  </React.StrictMode>,
+)
